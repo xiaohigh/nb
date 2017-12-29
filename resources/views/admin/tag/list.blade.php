a133959e4f3e8e6a2f97816ca45b71bc48abfb50
xiaohigh <liximing2011@gmail.com>
2017-12-29 07:08:51 +0000

    fix delete bug

--- resources/views/admin/tag/list.blade.php
+++ resources/views/admin/tag/list.blade.php
@@ -17,7 +17,7 @@
                         <tr>
                             <td>{{$v->id}}</td>
                             <td>{{$v->name}}</td>
-                            <td><a href="{{url('/tag/'.$v->id.'/edit')}}" class="btn btn-info btn-sm">修改</a>&nbsp;&nbsp;<a href="{{url('/tag/'.$v->id)}}" class="btn btn-danger btn-sm del">删除</a></td>
+                            <td><a href="{{url('/tag/'.$v->id.'/edit')}}" class="btn btn-info btn-sm">修改</a>&nbsp;&nbsp;<a href="{{url('/tag/'.$v->id)}}" class="btn btn-danger btn-sm" onclick="return del(this)">删除</a></td>
                         </tr>
                     @endforeach
 
@@ -27,5 +27,39 @@
         </div>
     </div>
 @endsection
+
+@section('js')
+<script>
+//点击删除按钮删除元素
+function del(obj){
+    var obj = $(obj);
+    //获取tr元素
+    var tr = obj.parents('tr');
+    //确认
+    var res = confirm('确定要删除么?');
+    if(res) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        //获取url
+        var url = obj.attr('href');
+        var data = {'_method':'delete'};
+        $.post(url, data, function(data){
+            if(data.status == 1) {
+                tr.remove();
+                remind('删除成功');
+            }else{
+                remind('删除失败');
+            }
+        },'json');
+    }else{
+
+    }
+    return false;
+}
+</script>
+@stop
 
 
